8be0f7f8e9528e7e126af521323acffd82383c4b
domingoUnican <domingo.domingogomez@gmail.com>
2022-08-03 16:17:32 +0200

    Added the source files

---
+++ Makefile
@@ -0,0 +1,61 @@
+
+##### Configurable options:
+
+## Compiler:
+CC=gcc
+#CC=cc
+
+## Compiler flags:
+
+# GCC:  (also -march=pentium etc, for machine-dependent optimizing)
+CFLAGS=-Wall -O3 -fomit-frame-pointer -funroll-loops
+
+# GCC w/ debugging:
+#CFLAGS=-Wall -g -DINLINE=
+
+# Compaq C / Digital C:
+#CFLAGS=-arch=host -fast
+
+# SunOS:
+#CFLAGS=-fast
+
+## Program options:
+
+# Enable long options for cl (eg. "cl --help"), comment out to disable.
+# Requires getopt_long(3)  (a GNU extension)
+LONGOPTS = -DENABLE_LONG_OPTIONS
+
+
+##### End of configurable options
+
+
+all: Paley CoPaley
+
+
+testcases: testcases.o cliquer.o graph.o reorder.o
+	$(CC) $(LDFLAGS) -o $@ testcases.o cliquer.o graph.o reorder.o
+
+Paley: Paley.o cliquer.o graph.o reorder.o
+	$(CC) $(LDFLAGS) -o $@ Paley.o cliquer.o graph.o reorder.o
+
+CoPaley: CoPaley.o cliquer.o graph.o reorder.o
+	$(CC) $(LDFLAGS) -o $@ CoPaley.o cliquer.o graph.o reorder.o
+
+
+cl.o testcases.o cliquer.o graph.o reorder.o: cliquer.h set.h graph.h misc.h reorder.h Makefile cliquerconf.h
+
+Paley.o: Paley.c
+	$(CC) $(CFLAGS) $(LONGOPTS) -o $@ -c $<
+
+CoPaley.o: CoPaley.c
+	$(CC) $(CFLAGS) $(LONGOPTS) -o $@ -c $<
+
+clean:
+	rm -f *.o *~ cl testcases
+
+backup:
+	mkdir "`date "+backup-%Y-%m-%d-%H-%M"`" 2>/dev/null || true
+	cp * "`date "+backup-%Y-%m-%d-%H-%M"`"  2>/dev/null || true
+
+test: testcases
+	./testcases
---
+++ hamming.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "cliquer.h"
+
+
+
+static boolean print_clique(set_t s, graph_t *g, clique_options *opts);
+
+static boolean print_clique(set_t s, graph_t *g, clique_options *opts) {
+  if (set_return_next(s,0) == 1)
+    {
+      printf("New clique found:\n");
+      set_print(s);
+      printf("*****\n");
+    }
+  return TRUE;
+}
+
+/* Function for determining wether the substraction of two numbers is an square */
+int is_square_mod_m(int c1, int c2, int m) {
+  int number = (m + c1-c2)%m ;
+  int temp  = 0;
+  for (int i=1; i<m; i++)
+    {
+      temp = (i*i)%m;
+      if (temp == number || temp == m-number)
+	{
+	return 0;
+	}
+    }
+  return 1;
+}
+int main(int argc, char *argv[]) {
+    int i, j;
+    int exponent, modulo;
+    int order;
+    graph_t *g;
+	
+    if (argc!=3) {
+        fprintf(stderr,"%s <exponent> <modulo>\n",argv[0]);
+        return 1;
+    }
+    exponent = atoi(argv[1]);
+    modulo = atoi(argv[2]);
+    /* no timer */
+    //clique_default_options->time_function = NULL; 
+
+    order = modulo;
+    g = graph_new(order);
+    for (i = 1; i < order; i++)
+        for (j = i+1; j < order; j++)
+	  if(is_square_mod_m(i,j,modulo) == 1)
+	    {
+	      GRAPH_ADD_EDGE(g,i,j);
+	    }
+    clique_default_options->output=stderr;
+    clique_default_options->user_function = print_clique;
+    int number_cliques = clique_find_all(g,0,0,TRUE,clique_default_options);
+    printf("number of them:%d\n",number_cliques);
+    return 0;
+}
